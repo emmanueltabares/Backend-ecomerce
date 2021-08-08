46f7ddeb33f1fcae3ca3acca0ef72eeee2ee5954
Emmanuel Tabares <e.tabares20@gmail.com>
2021-08-08 16:38:56 -0300

    Utilizando PugJS

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import router from "./routes/recursos";
 import path from "path";
-import handlebars from "express-handlebars";
 
 const port = 3000;
 const app = express();
@@ -14,21 +13,12 @@ server.on("error", (error) =>
   console.log("Ocurrió un problema en el servidor ", error)
 );
 
-/* const publicPath = path.resolve(__dirname, "./public");
-app.use(express.static(publicPath)); */
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
 
-const layoutFolderPath = path.resolve(__dirname, './views/layouts');
-const defaultLayerPath = path.resolve(__dirname, './views/layouts/index.handlebars')
-const partialFolderPath = path.resolve(__dirname, './views/partial')
-
-app.set('view engine', 'handlebars');
-app.engine('handlebars', 
-  handlebars({
-    layoutsDir: layoutFolderPath,
-    defaultLayout: defaultLayerPath,
-    partialsDir: partialFolderPath
-  })
-);
+app.set('view engine', 'pug');
+const viewsFolderPath = path.resolve(__dirname, './views');
+app.set('views', viewsFolderPath)
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
