e6a543933383069eea63b3694396b7c3cb904c30
Emmanuel Tabares <e.tabares20@gmail.com>
2021-10-19 17:00:37 -0300

    Update server.ts

--- src/services/server.ts
+++ src/services/server.ts
@@ -7,12 +7,9 @@ import cookieParser from 'cookie-parser';
 import MongoStore from 'connect-mongo';
 import passport from '../middleware/auth';
 import 'dotenv/config'
-import { fork } from 'child_process';
 
 const app = express();
 const timeSession = 1000 * 60 * 60;
-
-const scriptPath = path.resolve(__dirname, '../utils/calculo.ts');
 
 const publicFolderPath = path.resolve(__dirname, '../../public');
 app.use(express.static(publicFolderPath));
@@ -43,19 +40,6 @@ app.use(passport.session());
 
 app.use('/api', apiRouter);
 
-app.get('/randoms', (req, res) => {
-  const computo = fork(scriptPath);
-  computo.send('start');
-  computo.on('message', (sum) => {
-    res.json({
-      resultado: sum,
-    });
-  });
-});
-
-app.get('/info', (req, res) => {
-  
-})
 
 app.set('view engine', 'pug');
 const viewsFolderPath = path.resolve(__dirname, '../views');
